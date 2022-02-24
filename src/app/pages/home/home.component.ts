4bebf250ece2795ec30aba3691c125b06e59f571
Dasha <dasha.kotova.mail@gmail.com>
2022-02-23 20:16:34 -0500

    add front page layout

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderDetailsService } from 'src/app/services/order-details.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public foodData: any;
+
+  constructor(private service: OrderDetailsService) {}
 
   ngOnInit(): void {
+    this.foodData = this.service.foodDetails;
   }
 
 }
